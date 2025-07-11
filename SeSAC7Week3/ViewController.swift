409dc1803465705339fce71d1ddc2a81f9789b2d
JIMIN-iOSDev <tig05015@naver.com>
2025-07-11 11:16:22 +0900

    숫자 4를 9로 변경

--- SeSAC7Week3/ViewController.swift
+++ SeSAC7Week3/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         print(1)
         print(2)
         print(3)
-        print(4)
+        print(9)
         print(5)
         print(6)
         print(7)
